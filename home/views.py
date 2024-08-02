44752d105487954caa0f92016b6e4c25cc98034c
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-08-02 16:49:00 +0530

    searchrank

--- home/views.py
+++ home/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 from home.models import Product
-from django.contrib.postgres.search import SearchVector , SearchQuery
+from django.contrib.postgres.search import SearchVector , SearchQuery , SearchRank
 
 # Create your views here.
 def index(request):
     search = request.GET.get('search')
     if search:
         query = SearchQuery(search)
+        vector = SearchVector('title' ,'category','brand','sku')
+        rank = SearchRank(vector,query)
         results = Product.objects.annotate(
-            search = SearchVector('title') + SearchVector('category') +  SearchVector('description')
-        ).filter(search = query)
+            rank = rank 
+        ).filter(rank__gte=.06).order_by('-rank')
     else:
         results = Product.objects.all()
+
+    for result in results:
+        print(result.rank)
+        
 
     return render(request, 'index.html', {'results': results , 'search' : request.GET.get('search')})
