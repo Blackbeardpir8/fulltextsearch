966d2367dee6685ca5ac4968502ccdb250549615
Blackbeardpir8 <deepakdk15yadav@gmail.com>
2024-08-02 16:28:03 +0530

    searchQuery

--- home/views.py
+++ home/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from home.models import Product
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector , SearchQuery
 
 # Create your views here.
 def index(request):
     search = request.GET.get('search')
     if search:
+        query = SearchQuery(search)
         results = Product.objects.annotate(
             search = SearchVector('title') + SearchVector('category') +  SearchVector('description')
-        ).filter(search = search)
+        ).filter(search = query)
     else:
         results = Product.objects.all()
 
